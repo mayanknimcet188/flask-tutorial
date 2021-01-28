44e186e8560b4ddbf6b663b56b7a81bcae51bdac
mayanknimcet188 <205119053@nitt.edu>
2021-01-28 16:39:33 +0530

    Added User and Role model

--- hello.py
+++ hello.py
@@ -20,14 +20,16 @@ moment = Moment(app)
 
 #models
 class Role(db.Model):
+    __tablename__ = 'roles'
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(64),unique=True)
-    users = db.relationship('User',backref='role')
+    users = db.relationship('User',backref='role',lazu='dynamic')
 
     def __repr__(self):
         return '<Role %r>' % self.name
 
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key =True)
     username = db.Column(db.String(64),unique=True,index=True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
@@ -43,12 +45,18 @@ class NameForm(FlaskForm):
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name!=form.name.data:
-            flash('Looks like you have changed the name!')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(name=form.name.data)
+            db.session.add()
+            db.session.commit()
+            session['known']=False
+        else:
+            session['known'] = True
         session['name']=form.name.data
+        form.name.data=''
         return redirect(url_for('index'))
-    return render_template('index.html',form=form,name=session.get('name'))
+    return render_template('index.html',form=form,name=session.get('name'),known=session.get('known',False))
 
 @app.route('/user/<name>')
 def user(name):
